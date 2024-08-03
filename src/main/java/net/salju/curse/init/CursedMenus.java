d4077b8e2b41cf99179f968b122b0e2f170622f1
Salju <61861078+Jusey1@users.noreply.github.com>
2024-08-03 12:35:49 -0400

    Delete src/main/java directory

--- src/main/java/net/salju/curse/init/CursedMenus.java
+++
@@ -1,15 +0,0 @@
-package net.salju.curse.init;
-
-import net.salju.curse.gui.CurseGuiMenu;
-import net.salju.curse.CurseMod;
-
-import net.minecraftforge.registries.RegistryObject;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.common.extensions.IForgeMenuType;
-import net.minecraft.world.inventory.MenuType;
-
-public class CursedMenus {
-	public static final DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.MENU_TYPES, CurseMod.MODID);
-	public static final RegistryObject<MenuType<CurseGuiMenu>> CURSE_GUI = REGISTRY.register("curse_gui", () -> IForgeMenuType.create(CurseGuiMenu::new));
-}
