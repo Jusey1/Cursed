514c74462f0959b069b149db89defde1db096757
Salju <61861078+Jusey1@users.noreply.github.com>
2024-11-11 14:13:04 -0500

    Add files via upload

--- src/main/java/net/salju/curse/mixins/ItemMixin.java
+++ src/main/java/net/salju/curse/mixins/ItemMixin.java
@@ -1,5 +1,6 @@
 package net.salju.curse.mixins;
 
+import net.salju.curse.init.CursedConfig;
 import net.salju.curse.init.CursedData;
 import net.salju.curse.item.component.CursedItem;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,6 +21,31 @@ public abstract class ItemMixin {
 		CursedItem data = stack.get(CursedData.CURSED);
 		if (data != null) {
 			list.add(Component.translatable("item.curse.tooltip." + data.getCursedStatus()).withStyle(data.getCursedStatus() ? ChatFormatting.DARK_RED : ChatFormatting.GOLD));
+			if (data.getCursedStatus()) {
+				list.add(Component.literal(""));
+				if (flag.hasShiftDown()) {
+					if (CursedConfig.DEATH.get() > 100) {
+						list.add(Component.translatable("item.curse.tooltip.damage", Component.literal(Integer.toString(CursedConfig.DEATH.get() - 100)).withStyle(ChatFormatting.GOLD), Component.literal("%").withStyle(ChatFormatting.GOLD)).withStyle(ChatFormatting.DARK_PURPLE));
+					}
+					if (CursedConfig.ANGRY.get()) {
+						list.add(Component.translatable("item.curse.tooltip.neutrals").withStyle(ChatFormatting.DARK_PURPLE));
+					}
+					if (CursedConfig.FIRE.get()) {
+						list.add(Component.translatable("item.curse.tooltip.fire").withStyle(ChatFormatting.DARK_PURPLE));
+					}
+					if (CursedConfig.SLEEP.get()) {
+						list.add(Component.translatable("item.curse.tooltip.sleep").withStyle(ChatFormatting.DARK_PURPLE));
+					}
+					if (CursedConfig.DROPS.get()) {
+						list.add(Component.translatable("item.curse.tooltip.drops").withStyle(ChatFormatting.DARK_PURPLE));
+					}
+					if (CursedConfig.EXP.get() > 100) {
+						list.add(Component.translatable("item.curse.tooltip.xp", Component.literal(Integer.toString(CursedConfig.EXP.get() - 100)).withStyle(ChatFormatting.GOLD), Component.literal("%").withStyle(ChatFormatting.GOLD)).withStyle(ChatFormatting.DARK_PURPLE));
+					}
+				} else {
+					list.add(Component.translatable("item.curse.tooltip.shift").withStyle(ChatFormatting.DARK_PURPLE));
+				}
+			}
 		}
 	}
 }
